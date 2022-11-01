baf811bdae6cc9acb40cef27773e9c590b49b6d5
cvetolinkaaaaa <c.evseeva05@mail.ru>
2022-11-02 00:09:58 +0300

    Update Game.java

--- classwork/Game.java
+++ classwork/Game.java
@@ -1,17 +1,15 @@
 public class Game {
     static void fight (Person person1, Person person2) {
         while (person1.hp>0 && person2.hp>0) {
-
             person1.hit(person2);
-            if (person1.hp <= 0 || person2.hp <= 0) {
-                break;
-            }
+            System.out.println(person2.name + " " + " " + person2.hp);
             person2.hit(person1);
+            System.out.println(person1.name + " " + " " + person1.hp);
         }
         if (person1.hp <= 0) {
-            System.out.println(person1.name);
-        } else if (person2.hp <= 0) {
             System.out.println(person2.name);
+        } else if (person2.hp <= 0) {
+            System.out.println(person1.name);
         }
     }
 }
